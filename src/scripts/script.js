7310d869cfa5c7a993573bfc272a62888e9c206b
shizakira <ikirachn@gmail.com>
2024-07-23 11:16:14 +0500

    13662: Добавит JS скрипты

---
+++ src/scripts/script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    var menu = document.getElementById("header__navbar-mobile");
+    if (menu.classList.contains("header__navbar-mobile--show")) {
+        menu.classList.remove("header__navbar-mobile--show");
+        setTimeout(function () {
+            menu.style.display = "none";
+        }, 100);
+    } else {
+        menu.style.display = "flex";
+        setTimeout(function () {
+            menu.classList.add("header__navbar-mobile--show");
+        }, 10);
+    }
+}
