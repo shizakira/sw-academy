7f7393496cc7bf1406f89ff993a35e6765027c0e
shizakira <ikirachn@gmail.com>
2024-07-23 18:19:57 +0500

    13662: фиксит info

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,14 +1,14 @@
 function toggleMenu() {
     let menu = document.getElementById("header__navbar-mobile");
-    if (menu.classList.contains("header__navbar-mobile--show")) {
-        menu.classList.remove("header__navbar-mobile--show");
+    if (menu.classList.contains("header__navbar-mobile_show")) {
+        menu.classList.remove("header__navbar-mobile_show");
         setTimeout(function () {
             menu.style.display = "none";
         }, 100);
     } else {
         menu.style.display = "flex";
         setTimeout(function () {
-            menu.classList.add("header__navbar-mobile--show");
+            menu.classList.add("header__navbar-mobile_show");
         }, 10);
     }
 }
